6447d2670fcde4004eebc004c55a42a89ab26e97
zhaozz-lab <18046506891@163.com>
2019-10-22 19:04:34 +0800

    modify SLList,add sentinel

--- SLList.java
+++ SLList.java
@@ -12,45 +12,100 @@ public class SLList{
                 next = n;
 	        }
          }
+    private IntNode sentinel;
     private IntNode first;
-	public SLList(int x){
-	first = new IntNode(x,null);
+
+    private int size;
+
+
+	// public SLList(int x){
+	//     first = new IntNode(x,null);
+	//     size = 1;
+	// }
+
+
+	// create null list
+	public SLList(){
+		first = null;
+		sentinel = null;
+		size = 0;
 	}
     
+
+    // public void addLast(int x){
+    // 	IntNode p = first;
+    // 	while(p.next != null){
+    // 		p = p.next;
+    // 	}
+    // 	p.next = new IntNode(x,null);
+    // }
+    
+    // public void addLast(int x){
+    // 	size += 1;
+    	
+    // 	if (first == null) {
+    // 		first = new IntNode(x,null);
+    // 		return
+    // 	}
+
+    // 	while (p.next != null){
+    // 		p = p.next;
+    // 	}
+    // 	p.next = new IntNode(x,null);
+    	
+    // }
+
+
+    // add sentinel node
     public void addLast(int x){
-    	IntNode p = first;
-    	while(p.next != null){
-    		p = p.next;
-    	}
-    	p.next = new IntNode(x,null);
+    	size += 1; 
+    	
+    	IntNode p = sentinel;
+        while (p.next != null) {
+            p = p.next;
+        }
+
+        p.next = new IntNode(x, null);
+    	
     }
+
+
 	public void addFirst(int x){
 		first = new IntNode(x,first);
+		sentinel = first;
+		size += 1;
 	}
+
 
 	public int getFirst(){
 		return first.item;
 	}
 
+
 	private int size(IntNode p){
-		if (p.next == null){
-			return 1;
-		}
-		else
-			return 1 + size(p.next);
+		// if (p.next == null){
+		// 	return 1;
+		// }
+		// else
+		// 	return 1 + size(p.next);
+		return size;
 	}
+
 
     public int size(){
     	return size(first);
     }
+    
+
     public static void main(String[] args){
-    	SLList L = new SLList(10);
+    	SLList L = new SLList();
     	L.addFirst(20);
-    	// System.out.println(L.getFirst());
+    	
         L.addLast(3);
-        L.addFirst(20);
-        L.addLast(3);
+        L.addFirst(10);
+        // L.addLast(3);
         System.out.println(L.size());
+        System.out.println(L.getFirst());
     }
 }
 
